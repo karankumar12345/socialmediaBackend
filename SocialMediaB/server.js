61e700eb57205b057d4de1731c39f4c9c29660fa
karan <karankumar2004122@gmail.com>
2024-06-15 14:12:41 +0530

    first commit

--- SocialMediaB/server.js
+++ SocialMediaB/server.js
@@ -20,13 +20,12 @@ cloudinary.config({
   });
 
   app.use(express.json({ limit: '50mb' }));
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-lwhq.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
-    }
-));
+  app.use(cors({
+    origin: ["https://deploy-mern-lwhq.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 // Routes
