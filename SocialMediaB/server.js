58719e83215380426b669cb217fb510bbe990e82
karan <karankumar2004122@gmail.com>
2024-06-16 19:32:48 +0530

    first commit

--- SocialMediaB/server.js
+++ SocialMediaB/server.js
@@ -2,41 +2,40 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDb = require('./config/database');
 const postRouter = require('./routes/post');
-const userRouter=require("./routes/User");
+const userRouter = require('./routes/User');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-dotenv.config({ path: '../SocialMediaB/config/config.env' });
 const cloudinary = require("cloudinary");
 const router = require('./routes/Story');
+
+dotenv.config({ path: '../SocialMediaB/config/config.env' });
 
 const app = express();
 
 // Middleware
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+});
 
-  app.use(express.json({ limit: '50mb' }));
-  app.use(cors());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+
 // Routes
-//simple hello 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 app.use('/api/v1/post', postRouter);
 app.use('/api/v2/user', userRouter);
-app.use('/api/v3/story',router)
+app.use('/api/v3/story', router);
 
 connectDb();
 
-const PORT = process.env.PORT||8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server started at Port ${PORT}`);
