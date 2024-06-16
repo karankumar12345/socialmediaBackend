67b851de2f94abad5d4a288d4b9077c79dec3666
karan <karankumar2004122@gmail.com>
2024-06-16 19:49:14 +0530

    first commit

--- SocialMediaB/config/database.js
+++ SocialMediaB/config/database.js
@@ -8,9 +8,7 @@ const connectDb = async () => {
 
         const data = await mongoose.connect(process.env.DB_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
         });
 
         console.log(`Connected Successfully: ${data.connection.host}`);
