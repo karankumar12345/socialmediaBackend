773e2855dc5f3ea65ff31f2cedff778d9e031198
karan <karankumar2004122@gmail.com>
2024-06-16 19:34:31 +0530

    first commit

--- SocialMediaB/config/database.js
+++ SocialMediaB/config/database.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 
-const connectDb = () => {
-    if (!process.env.DB_URL) {
-        console.error("DB_URL environment variable is not set.");
-        return;
-    }
+const connectDb = async () => {
+    try {
+        if (!process.env.DB_URL) {
+            throw new Error("DB_URL environment variable is not set.");
+        }
 
-    mongoose.connect(process.env.DB_URL).then((data) => {
+        const data = await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
         console.log(`Connected Successfully: ${data.connection.host}`);
-    })
-}
+    } catch (error) {
+        console.error(`Error connecting to the database: ${error.message}`);
+        process.exit(1); // Exit the process with a failure code
+    }
+};
 
 module.exports = connectDb;
